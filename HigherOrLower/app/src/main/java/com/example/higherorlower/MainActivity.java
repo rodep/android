80e2f230e0e158f6cc7cf6f01359f567da36685e
rodrigoep01 <rodrigoep01@gmail.com>
2019-02-01 15:30:24 -0200

    java file intial development

--- HigherOrLower/app/src/main/java/com/example/higherorlower/MainActivity.java
+++ HigherOrLower/app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -2,8 +2,33 @@ package com.example.higherorlower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+/* edit field created for manipulation, random number created. Test if random number works. */
+
+    public  void guessNumber(View view){
+
+        Random rand = new Random();
+        int randomNum = rand.nextInt((20 - 1) + 1) + 1;
+
+        EditText guessedNumberField = (EditText) findViewById(R.id.editTextGuessNumber);
+
+      // Toast.makeText(getApplicationContext(), randomNum.toString(), Toast.LENGTH_LONG).show();
+
+
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
