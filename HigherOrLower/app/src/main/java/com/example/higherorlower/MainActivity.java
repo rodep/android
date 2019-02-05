937ddbf94f098b1b3b1a1f63e3b671ff448e4661
rodrigoep01 <rodrigoep01@gmail.com>
2019-02-05 20:20:04 -0200

    H&L: Crippled Prototype

--- HigherOrLower/app/src/main/java/com/example/higherorlower/MainActivity.java
+++ HigherOrLower/app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -1,51 +1,65 @@
-package com.example.higherorlower;
+        package com.example.higherorlower;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-import java.util.Random;
-
-
-public class MainActivity extends AppCompatActivity {
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.Toast;
+        import java.util.Random;
 
 
+        public class MainActivity extends AppCompatActivity {
 
-    public  void onClick(View view){
+            // first issue: use the random number only one time and then guess
+            // check why log is repeating the prints: smaller, smaller.
+            public  void guessTheNumber(View view){
+
+                EditText guessedNumberField;
+                guessedNumberField = findViewById(R.id.editTextGuessNumber);
+
+                String text = guessedNumberField.getText().toString();
+                int guessedNum = new Integer(text).intValue();
+
+                int randNum = generateRandomNumber();
+
+                for(int i=0; i<= 20; i++){
+
+                    if(randNum == guessedNum){
+                        System.out.println("guessed right!");
+                    }
+
+                    if(randNum > guessedNum) {
+                         System.out.println("bigger");
+                    }
+
+                    if(randNum < guessedNum) {
+                        System.out.println("smaller");
+                    }
+                }
+
+                System.out.println("end of for loop");
+
+            }
+
+            public int generateRandomNumber(){
+
+                Random rand = new Random();
+                Integer  randomNum = rand.nextInt((20 - 1) + 1) + 1;
+                System.out.println(randomNum);
+
+                return randomNum;
+            }
+
+                @Override
+                protected void onCreate(Bundle savedInstanceState) {
+                    super.onCreate(savedInstanceState);
+                    setContentView(R.layout.activity_main);
+                    EditText guessedNumberField;
+                }
 
 
-        int randomNum = generateRandomNumber();
 
-        EditText guessedNumberField;
-        guessedNumberField = findViewById(R.id.editTextGuessNumber);
-        
-
-
-    }
-
-
-    public int generateRandomNumber(){
-
-        Random rand = new Random();
-        int randomNum = rand.nextInt((20 - 1) + 1) + 1;
-        System.out.println(randomNum);
-
-        return randomNum;
-    }
-
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            EditText guessedNumberField;
-        }
-
-
-
-    }
+            }
 
